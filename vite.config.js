fd9b17f73fce4c0e17e87b2235b308c2ab6c0e90
nico <nicolasalejocampos@outlook.es>
2025-12-10 11:06:56 -0300

    aa

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,13 @@ import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-    plugins: [react()],
-    server: {
-        proxy: {
-            "/api": {
-                target: "http://localhost:3000",
-                changeOrigin: true,
-            },
-        },
+  plugins: [react()],
+  server: {
+    proxy: {
+      "/api": {
+        target: process.env.BASE_URL,
+        changeOrigin: true,
+      },
     },
+  },
 });
