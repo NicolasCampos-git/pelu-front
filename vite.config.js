b89de1b51af7f1f2e36b1008060d417fbc114f33
nico <nicolasalejocampos@outlook.es>
2025-12-10 11:24:52 -0300

    aa

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,18 @@
 import react from "@vitejs/plugin-react-swc";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      "/api": {
-        target: process.env.BASE_URL,
-        changeOrigin: true,
+export default defineConfig(({ command, mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    plugins: [react()],
+    server: {
+      proxy: {
+        "/api": {
+          target: env.VITE_API_URL || "https://api-turnos-k4mc.onrender.com",
+          changeOrigin: true,
+        },
       },
     },
-  },
+  };
 });
